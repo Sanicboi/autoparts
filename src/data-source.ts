c32d6248e9db9c78b379324496585a777ad3b8ac
sanicboi <alexandrzezekalo@gmail.com>
2026-07-07 22:14:30 +0200

    Formatting & Docker

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,19 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entities/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entities/User";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "root",
-    password: "password",
-    database: "typeorm",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: +process.env.POSTGRES_PORT!,
+  username: process.env.POSTGRES_USER!,
+  password: process.env.POSTGRES_PASSWORD!,
+  database: process.env.POSTGRES_DB!,
+  synchronize: true,
+  logging: Boolean(process.env.PROD!),
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
+
+export const manager = AppDataSource.manager;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entities/User"
+import "dotenv/config";
+import { AppDataSource } from "./data-source";
+import express from "express";
 
-AppDataSource.initialize().then(async () => {
+const app = express();
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+AppDataSource.initialize()
+  .then(async () => {
+    app.get("*splat", async (req, res) => {
+      res.status(200).end();
+    });
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+    app.listen(8080);
+  })
+  .catch((error) => console.log(error));
