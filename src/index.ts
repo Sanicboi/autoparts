928bd1dfcabc64bdbb86e8728e231e4ea451a453
sanicboi <alexandrzezekalo@gmail.com>
2026-07-07 23:24:26 +0200

    binding

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import { AppDataSource } from "./data-source";
 import express from "express";
+import auth from "./routers/auth";
 
 export const app = express();
 app.use(express.json());
 
 AppDataSource.initialize()
   .then(async () => {
+    app.use(auth);
+
     app.get("*splat", async (req, res) => {
       res.status(200).end();
     });
