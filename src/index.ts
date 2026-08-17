78047e778c862c4a0588d19ac3210ab39f75095b
sanicboi <alexandrzezekalo@gmail.com>
2026-08-17 18:07:41 +0200

    added static serving

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,13 @@ import * as exist from "./parsers/exist";
 import * as autodoc from "./parsers/autodoc";
 import puppeteer from "puppeteer";
 import morgan from "morgan";
+import path from "path";
 
 export const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(auth);
+app.use(express.static(path.join(process.cwd(), 'dist')));
 
 // app.get("*splat", async (req, res) => {
 //   res.status(200).end();
